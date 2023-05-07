e0accbde13914457daee3ed687b1a3be8f44a381
bilal Ahmed <bilalo17141222@gmail.com>
2023-05-07 23:27:12 +0300

    working on the frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import image1 from '../images/hero-image.jpg';
 import image2 from '../images/hero1.jpg';
 import image3 from '../images/hero2.jpg';
+import { FcSearch , FcCalendar} from "react-icons/fc";
+import { MdOutlinePayment } from "react-icons/md";
+import { GiBabyfootPlayers } from "react-icons/gi";
 
 const Home = () => {
   return (
@@ -26,9 +29,10 @@ const Home = () => {
      </div>
     </section>
 
-    <section className='w-full h-96 bg-lightGreen ' >
-     <div className='flex-row  items-center justify-center bg-red-800 lg:max-w-7xl lg:mx-auto lg:h-96 lg:flex lg:row-auto lg:items-center lg:justify-between ' >
-     <div className=' w-11/12 mx-auto  lg:w-72 h-80 lg:mx-0 bg-amber-700' >
+    <section className='w-full h-fit bg-lightGreen ' >
+      <h1 className=' text-center text-5xl py-3 ' >Find Your Turfs</h1>
+     <div className='  space-y-4 bg-red-800 lg:max-w-7xl lg:mx-auto lg:h-96 lg:space-y-0 lg:flex lg:row-auto lg:items-center lg:justify-between ' >
+     <div className=' w-11/12 mx-auto   lg:w-72 h-80 lg:mx-0 bg-amber-700' >
       <h1>turf 1</h1>
      </div>
      <div className='w-11/12 mx-auto lg:w-72 h-80 bg-lightGreen lg:mx-0'>
@@ -39,6 +43,52 @@ const Home = () => {
      </div>
      </div>
     </section>
+
+    <section className='h-full bg-white' >
+      <h2 className=' text-center text-3xl py-2  lg:text-5xl ' >Book Your Game within Minutes</h2>
+      <span className=' bg-grey mx-auto space-y-5 lg:max-w-7xl lg:flex justify-between lg:space-y-0 py-5 rounded-lg ' > 
+        <div className='w-11/12 h-40 mx-auto lg:w-60 lg:h-60 flex lg:flex-col lg:justify-center items-center   px-6 lg:space-y-3 flex-row rounded-md ' >
+        <div className=' bg-grey  w-36  h-24 flex justify-center items-center rounded-lg ' >
+        <FcSearch size='4em' />
+        </div>
+        <div className=' w-48 h-24  flex flex-col justify-center items-center    ' >
+          <h1 className=' text-2xl  font-bold ' >Find</h1>
+          <p className=' font-normal' >A nearby Stadium</p>
+        </div>
+        </div>
+        <div className='w-11/12 h-40 mx-auto lg:w-60 lg:h-60 flex lg:flex-col lg:justify-center px-6 items-center lg:space-y-3 rounded-md ' >
+          <div className='bg-grey  w-36  h-24 flex justify-center items-center rounded-lg' >
+            <FcCalendar size="4em" />
+          </div>
+          <div className='w-48 h-24  flex flex-col justify-center items-center' >
+            <h1 className=' text-2xl font-bold ' >Choose</h1>
+            <p className='font-normal text-center' >The date,time,duration and pitch size</p>
+          </div>
+        </div>
+        <div className='w-11/12 h-40 mx-auto  lg:w-60 lg:h-60 flex lg:flex-col lg:justify-center px-6 items-center lg:space-y-3 rounded-md ' >
+          <div className='bg-grey  w-36  h-24 flex justify-center items-center rounded-lg' >
+            <MdOutlinePayment size="4em" color='green' />
+         </div>
+         <div className='w-48 h-24  flex flex-col justify-center items-center' >
+          <h1 className='text-2xl font-bold' >Pay</h1>
+          <p className='font-normal text-center' >Online using Mpesa</p>
+         </div>
+        </div>
+        <div className='w-11/12 h-40 mx-auto lg:w-60 lg:h-60 flex lg:flex-col lg:justify-center px-6 items-center lg:space-y-3 rounded-md' >
+          <div className='bg-grey  w-36  h-24 flex justify-center items-center rounded-lg' >
+            <GiBabyfootPlayers size="4em" color='green' />
+          </div>
+          <div className=' w-48 h-24  flex flex-col justify-center items-center' >
+            <h1 className='text-2xl font-bold' >Play</h1>
+            <p className='font-normal text-center ' >Come show your skills</p>
+          </div>
+        </div>
+      </span>
+    </section>
+
+    <footer className=' bg-yellow-400 h-full ' >
+       
+    </footer>
 
 
     </body>
