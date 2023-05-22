da90f5cd8c506b8504376a3ff21872b00a949de7
bilal Ahmed <bilalo17141222@gmail.com>
2023-05-22 12:12:42 +0300

    update on the database

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 
 
-const db = new Sequelize("shikaGrao" , "root" , "" , {
-    host : "localhost",
+const db = new Sequelize("btr3y1liz4vh9p3b6eau" , "u6pu0jwuae3ombow" , "2erZc5WWa5o3SHj4QI1n" , {
+    host : "btr3y1liz4vh9p3b6eau-mysql.services.clever-cloud.com",
     dialect : "mysql"
 });
 
