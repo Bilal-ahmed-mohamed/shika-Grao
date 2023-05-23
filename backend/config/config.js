a7e045da5bc197950bfd97613a22ff4c19ddee4d
bilal Ahmed <bilalo17141222@gmail.com>
2023-05-23 09:44:57 +0300

    removed a bug

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 
 
-const db = new Sequelize("btr3y1liz4vh9p3b6eau" , "u6pu0jwuae3ombow" , "2erZc5WWa5o3SHj4QI1n" , {
-    host : "btr3y1liz4vh9p3b6eau-mysql.services.clever-cloud.com",
+const db = new Sequelize("shikagrao" , "root" , "" , {
+    host : "localhost",
     dialect : "mysql"
 });
 
