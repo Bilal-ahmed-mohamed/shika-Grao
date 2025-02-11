7d450f13e5555dd6bb12e77fe38f879ed67a7249
bilal_ahmed <105801544+Bilal-ahmed-mohamed@users.noreply.github.com>
2025-02-11 22:52:45 +0300

    worked on the booking functionality

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { format, addDays, subDays } from 'date-fns';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import moment from 'moment';
 
@@ -8,12 +8,18 @@ function Calendar() {
   
 
   const {id} = useParams();
+
+  const [title , setTitle] = useState('');
+  const [surface , setSurface] = useState('');
+  const [postcode , setPostcode] = useState('');
   const [numberOfPitches , setNumberOfPitches] = useState('');
   const [venue , setVenue] = useState('');
   const [startTime , setStartTime] = useState('');
   const [endTime , setCloseTime] = useState('');
   const [matchDuration , setMatchDuration] = useState('');
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [modalOpen , setModalIsOpen] = useState(false);
+  const [selectedTimeSlot, setSelectedTimeSlot] = useState(null);
 
 
   
@@ -23,6 +29,10 @@ function Calendar() {
     setStartTime(res.data.singleTurf.startTime);
     setCloseTime(res.data.singleTurf.closeTime);
     setMatchDuration(res.data.singleTurf.matchDuration); 
+    setVenue(res.data.singleTurf.venue);
+    setTitle(res.data.singleTurf.title);
+    setSurface(res.data.singleTurf.surface);
+    setPostcode(res.data.singleTurf.postcode);
    }
 
   const nextWeek = () => {
@@ -65,9 +75,15 @@ function Calendar() {
         </span>
         <div className="flex justify-around  flex-wrap h-auto w-auto  ">
           {timeSlots.map((slot, index) => (
-            <div key={index} className=" mb-3 bg-grey text-black py-2 px-4 rounded cursor-pointer lg:mb-0 ml-2">
+            <Link to="#"  onClick={(e) => {
+                   e.preventDefault()
+                   setSelectedTimeSlot(slot);
+                   setModalIsOpen(true);
+                   console.log(selectedTimeSlot);
+                   
+            }} key={index} className=" mb-3 bg-grey text-black py-2 px-4 rounded cursor-pointer lg:mb-0 ml-2">
               {slot}
-            </div>
+            </Link>
           ))}
         </div>
       </div>
@@ -75,10 +91,6 @@ function Calendar() {
   };
 
   
-
-
-  
-
   useEffect(() => {
     singleTurf();
    })
@@ -107,6 +119,44 @@ function Calendar() {
           </div>
         ))}
       </div>
+
+      { modalOpen && (
+        <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'>
+          <div className=' bg-slate-700 p-14 rounded-lg shadow-lg w-1/3 relative'>
+             <button onClick={() => setModalIsOpen(false)}
+               className='absolute top-3 right-4 text-gray-500 hover:text-gray-800 text-lg' >
+              &times;
+              
+             </button>
+
+             <div className=' bg-orange-300 '>
+              <p className="text-red-700">You selected the slot: <strong>{selectedTimeSlot}</strong></p>
+              <h1>{title}</h1>
+              <h2>{postcode}</h2>
+              <h3>{venue}</h3>
+              <h4>{matchDuration}</h4>
+             </div>
+            
+            <div className='mt-4  bg-green-400 flex justify-end'>
+
+            <button
+                onClick={() => setModalIsOpen(false)}
+                className="px-4 py-2 bg-gray-300 text-black rounded mr-2"
+              >
+                Cancel
+              </button>
+
+              <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                Confirm
+              </button>
+
+            </div>
+
+          </div>
+
+        </div>
+      ) }
+      
        
     </div>
   );
